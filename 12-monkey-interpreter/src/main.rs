553a398c7808fc5187a52f05ddc22888b2497d9c
Gus Sánchez <goosethedev@proton.me>
2025-04-10 17:06:19 -0400

    12: basic REPL

--- 12-monkey-interpreter/src/main.rs
+++ 12-monkey-interpreter/src/main.rs
@@ -1,5 +1,26 @@
 mod lexer;
 
+use std::io::{self, Write};
+
+use lexer::Lexer;
+
+const PROMPT: &str = ">> ";
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        // Print prompt
+        print!("{}", PROMPT);
+        io::stdout().flush().unwrap();
+
+        // Read a line from input
+        let mut line = String::new();
+        let stdin = io::stdin();
+        stdin
+            .read_line(&mut line)
+            .expect("Couldn't read from stdin");
+
+        let mut lexer = Lexer::new(&line);
+        let output = lexer.parse();
+        dbg!(output);
+    }
 }
